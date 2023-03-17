511250419ab1b258c5ba83e174223c98f0ab7fce
alexscarola <alsc3850@colorado.edu>
2023-03-16 18:09:28 -0600

    trying

--- Project4.2/Command.java
+++
@@ -1,9 +0,0 @@
-import java.io.Console;
-import java.util.Scanner;
-
-public interface Command{
-    public void execute();
-}
-
-
-//I think that I do this menu where ever, and then have switch statements triggering different command options in here
--- Project4.2/Exit.java
+++
@@ -1,9 +0,0 @@
-import java.io.Console;
-
-class Exit implements Command{
-    Console cons = System.console();
-    public void execute(){
-        cons.printf("We hope our service met your standards. Please come again!\n");
-        cons.printf("\n");
-    }
-}
